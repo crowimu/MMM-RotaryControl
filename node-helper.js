61cfbe6a9c8561c4f4b68476345b1068c5392195
crowimu <30473047+crowimu@users.noreply.github.com>
2025-03-02 14:43:33 +0100

    Update node-helper.js

--- node-helper.js
+++ node-helper.js
@@ -1,42 +1,47 @@
-'use strict';
-const NodeHelper = require('node_helper');
-const pigpio = require('pigpio');
+"use strict";
+const NodeHelper = require("node_helper");
+const pigpio = require("pigpio");
 const Gpio = pigpio.Gpio;
 
 module.exports = NodeHelper.create({
   start: function () {
-    console.log('[MMM-RotaryNav] Module started');
+    console.log("[MMM-RotaryNav] Module started");
     this.config = null;
-    this.initEncoder();
   },
 
   socketNotificationReceived: function (notification, payload) {
-    if (notification === 'CONFIG') {
+    if (notification === "CONFIG") {
       this.config = payload;
+      this.initEncoder();
     }
   },
 
   initEncoder: function () {
-    const clk = new Gpio(17, { mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP, edge: Gpio.EITHER_EDGE });
-    const dt = new Gpio(27, { mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP, edge: Gpio.EITHER_EDGE });
-    const sw = new Gpio(22, { mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP, edge: Gpio.FALLING_EDGE });
+    if (!this.config) {
+      console.error("[MMM-RotaryNav] Config not received!");
+      return;
+    }
+
+    const clk = new Gpio(this.config.clkPin, { mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP, edge: Gpio.EITHER_EDGE });
+    const dt = new Gpio(this.config.dtPin, { mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP, edge: Gpio.EITHER_EDGE });
+    const sw = new Gpio(this.config.swPin, { mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP, edge: Gpio.FALLING_EDGE });
 
     let lastState = clk.digitalRead();
 
-    clk.on('interrupt', (level) => {
+    clk.on("interrupt", (level) => {
       const dtState = dt.digitalRead();
       if (level !== lastState) {
-        this.sendSocketNotification(dtState ? 'ROTARY_RIGHT' : 'ROTARY_LEFT');
+        this.sendSocketNotification(dtState ? "ROTARY_RIGHT" : "ROTARY_LEFT");
         lastState = level;
       }
     });
 
-    sw.on('interrupt', () => {
-      this.sendSocketNotification('ROTARY_PRESS');
+    sw.on("interrupt", () => {
+      this.sendSocketNotification("ROTARY_PRESS");
     });
   },
 
   stop: function () {
-    console.log('[MMM-RotaryNav] Module stopped');
+    console.log("[MMM-RotaryNav] Module stopped");
   }
 });
